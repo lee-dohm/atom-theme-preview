81263c7ebf5f7e1887e062bc29642dc4a0880984
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-11-12 21:13:41 -0800

    Add about page

--- lib/theme_preview_web/controllers/page_controller.ex
+++ lib/theme_preview_web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule ThemePreviewWeb.PageController do
   use ThemePreviewWeb, :controller
 
+  def about(conn, _params) do
+    render(conn, "about.html")
+  end
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
 end
